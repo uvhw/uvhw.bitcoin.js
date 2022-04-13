05eb1f8483c5798406a766fdd5ef44f957f7521f
uvhw <uvhwuvhw@gmail.com>
2022-04-13 09:39:57 +0700

    Initialize patch-3

---
+++ src/qt/sendcoinsdialog.h
@@ -0,0 +1,57 @@
+#ifndef SENDCOINSDIALOG_H
+#define SENDCOINSDIALOG_H
+
+#include <QDialog>
+#include <QVariant>
+
+namespace Ui {
+    class SendCoinsDialog;
+}
+class WalletModel;
+class SendCoinsEntry;
+class SendCoinsRecipient;
+class OptionsModel;
+
+QT_BEGIN_NAMESPACE
+class QUrl;
+QT_END_NAMESPACE
+
+/** Dialog for sending bitcoins */
+class SendCoinsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SendCoinsDialog(QWidget *parent = 0);
+    ~SendCoinsDialog();
+
+    void setModel(WalletModel *model);
+
+    /** Set up the tab chain manually, as Qt messes up the tab chain by default in some cases (issue https://bugreports.qt-project.org/browse/QTBUG-10907).
+     */
+    QWidget *setupTabChain(QWidget *prev);
+
+    void setAddress(const QString &address);
+    void pasteEntry(const SendCoinsRecipient &rv);
+    bool handlePaymentRequest(const SendCoinsRecipient &recipient);
+
+public slots:
+    void clear();
+    void reject();
+    void accept();
+    SendCoinsEntry *addEntry();
+    void updateRemoveEnabled();
+    void setBalance(qint64 balance, qint64 unconfirmedBalance, qint64 immatureBalance);
+
+private:
+    Ui::SendCoinsDialog *ui;
+    WalletModel *model;
+    bool fNewRecipientAllowed;
+
+private slots:
+    void on_sendButton_clicked();
+    void removeEntry(SendCoinsEntry* entry);
+    void updateDisplayUnit();
+};
+
+#endif // SENDCOINSDIALOG_H
